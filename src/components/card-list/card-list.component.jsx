7980335197dc9e1c4540ca0bb0096c6ce4f81a90
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2023-02-12 00:24:17 +0400

    MonstersRolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import './card-list-styles.css'
+import '../card/card.component'
+import Card from "../card/card.component";
+class CardList extends Component{
+
+    render(){
+        console.log("render")
+        const { monsters } = this.props 
+        return(
+            <div className="card-list">
+                {
+                 monsters.map(monster =>{
+                    return(
+                     <Card monster ={monster}/>
+                 )})
+                }
+            </div>
+        )
+    }
+}
+
+export default CardList
